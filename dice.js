9ba083d968963aa247be088fee046c9a413e0086
Satish232021 <100710941+Satish232021@users.noreply.github.com>
2024-08-25 00:03:22 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImageSource = "./images/dice" + randomNumber1+ ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+var randomImageSource1 = "./images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource1);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 wins";
+    
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 wins"
+}
+
+else {
+    document.querySelector("h1").textContent = "draw";
+}
